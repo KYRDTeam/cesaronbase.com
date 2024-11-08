e39da0c20f8b4a9821fd75090d3800ba16d3ec85
Nam Nguyen <namngm91@gmail.com>
2024-11-08 17:16:51 +0700

    adding create-meme page

---
+++ assets/js/create-meme.js
@@ -0,0 +1,289 @@
+const mainTitle = new SplitText(".main-title", { type: "words" });
+const bgTxt = new SplitText(".background-txt span", { type: "words" });
+
+gsap.set(".item-head-front, .item-head-side, .item-head-side-more, .item-head-turned, .item-crown, .item-red-candle, .item-green-candle", { autoAlpha: 0, scale: .98, transformOrigin: '50% 50%' })
+
+/* ***************************************************************** */
+
+const introPFP = gsap.timeline({});
+
+introPFP
+    .to(".pfp-creator-section", {
+        duration: .1,
+        autoAlpha: 1,
+        ease: "power1.out"
+    })
+    .from(mainTitle.words, {
+        duration: .5,
+        stagger: {
+            each: 0.05,
+            from: "left",
+            grid: "auto"
+        },
+        autoAlpha: 0,
+        y: "4.4vh",
+        ease: "power1.out"
+    }, ">+.2")
+    .from(".upload-section-image", {
+        duration: .5,
+        autoAlpha: 0,
+        scale: 0.98,
+        y: "2.4vh",
+        ease: "power1.out"
+    }, ">-.3")
+    .from(".upload-elements-buttons", {
+        duration: .5,
+        autoAlpha: 0,
+        y: "2.4vh",
+        ease: "power1.out"
+    }, ">-.3");
+
+/* ***************************************************************** */
+
+const bgTxtPFP = gsap.timeline({ repeat: -1 });
+
+bgTxtPFP
+    .from(bgTxt.words, {
+        duration: .5,
+        stagger: 0.1,
+        autoAlpha: 0,
+        ease: "power1.out"
+    })
+    .fromTo(".background-txt-wrapper", {
+        x: "100vw"
+    }, {
+        duration: 50,
+        xPercent: -100,
+        ease: "none"
+    }, ">-.5")
+    .to(bgTxt.words, {
+        duration: .5,
+        stagger: {
+            each: 0.1,
+            from: 'end'
+        },
+        autoAlpha: 0,
+        ease: "power1.out"
+    }, ">-.5");
+
+/* ***************************************************************** */
+
+/* start --- after upload image this have to be deleted */
+
+
+
+$('.upload-section-image').on("click", function (e) {
+    $("body").addClass("view-user-image");
+    $("body").addClass("view-upload-elements");
+    $("body").addClass("view-upload-elements-buttons");
+});
+
+/* end --- after upload image this have to be deleted */
+
+/* ***************************************************************** */
+
+$('.ue-button').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        $(this).addClass("active");
+    } else {
+        $(this).removeClass("active");
+    }
+});
+
+$('.trigger-cesar-head-front').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-head-front', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-head-front', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-cesar-head-side').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-head-side', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-head-side', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-cesar-head-side-more').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-head-side-more', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-head-side-more', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-cesar-head-turned').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-head-turned', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-head-turned', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-crown').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-crown', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-crown', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-red-candle').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-red-candle', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-red-candle', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+$('.trigger-green-candle').on("click", function (e) {
+    if (!$(this).hasClass("active")) {
+        gsap.to('.item-green-candle', {
+            duration: .5,
+            autoAlpha: 0,
+            scale: .98,
+            ease: "power1.out"
+        });
+    } else {
+        gsap.to('.item-green-candle', {
+            duration: .5,
+            autoAlpha: 1,
+            scale: 1,
+            ease: "power1.out"
+        });
+    }
+});
+
+/* ***************************************************************** */
+
+/* handle logic to upload & export pfp */
+
+const fileInput = document.getElementById('pfp-input');
+const imagePreview = document.getElementById('pfp-preview');
+const exportBtn = document.getElementById('export-pfp');
+const canvas = document.getElementById('pfp-canvas');
+const ctx = canvas.getContext('2d');
+
+fileInput.addEventListener('change', (event) => {
+    console.log('something change here');
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            imagePreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
+
+// draw selected element to canvas and export it
+exportBtn.addEventListener('click', function() {
+    console.log('export clicked');
+    const image = document.getElementById('pfp-preview', { useCORS:true});
+    const overlayImage = document.getElementById('red-candle', { useCORS:true}); // Change this selector to the image you want to overlay
+
+    // Ensure both images are fully loaded before drawing
+    if (image.complete && overlayImage.complete) {
+        // Set canvas size to match the image size
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        // Draw the base image
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        // Draw the overlay image
+        ctx.drawImage(overlayImage, 0, 0, canvas.width, canvas.height); // Adjust position based on your layout
+
+        console.log('ok so far');
+
+        // Export the canvas as an image
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.crossOrigin = "anonymous";
+        link.download = 'pfp-image.png';
+        link.click();
+    } else {
+        console.error('Images are not fully loaded');
+    }
+});
+
+// using lib html2canvas to export image
+// exportBtn.addEventListener('click', function() {
+//     console.log('export clicked');
+//     const elementToCapture = document.querySelector('.upload-section-image'); // Change this selector to the element you want to capture
+
+//     html2canvas(elementToCapture, { useCORS: true, allowTaint: true,}).then(canvas => {
+//         // Export the canvas as an image
+//         const link = document.createElement('a');
+//         link.href = canvas.toDataURL('image/png');
+//         link.download = 'pfp-image.png';
+//         link.click();
+//     }).catch(error => {
+//         console.error('Error capturing the element:', error);
+//     });
+// });
